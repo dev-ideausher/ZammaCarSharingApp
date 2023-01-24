01ab7223d4dbd79b1a0fd1e36d50eb73ed9e3dbb
pushpam0 <kumarpushpam1995@gmail.com>
2023-01-24 11:33:13 +0530

    test

--- lib/app/modules/models/otp_used_class.dart
+++ lib/app/modules/models/otp_used_class.dart
@@ -2,5 +2,6 @@ class OtpNeededData{
   String mobileNumber;
   String veryFicationId;
   String smsCode;
+  String smCode="";
   OtpNeededData({this.mobileNumber="0",this.veryFicationId="0",this.smsCode="0"});
 }
